3f0feee8fb524b479ec34c044561f8b221d9d28d
libscode <lmtlibasse@gmail.com>
2022-09-02 10:31:39 +0000

    header sub heaer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursModule } from './modules/cours/cours.module';
+import { EvenementModule } from './modules/evenement/evenement.module';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CguComponent } from './pages/cgu/cgu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { Header2Component } from './components/header2/header2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CguComponent,
+    HeaderComponent,
+    CardsComponent,
+    Header2Component,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoursModule,
+    EvenementModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/cours/cours.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursRoutingModule } from './cours-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoursRoutingModule
+  ]
+})
+export class CoursModule { }
